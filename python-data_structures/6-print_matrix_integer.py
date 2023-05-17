e63241b1580c16e6558bbda40857d7dc60ae75c2
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-05-17 00:28:21 +0000

    6 printing matrix integer

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        row_len = len(row)
+        for element in range(row_len):
+            if(element != row_len - 1):
+                print("{:d}".format(row[element]), end=' ')
+            else:
+                print("{:d}".format(row[element]), end='')
+        print()
+        
