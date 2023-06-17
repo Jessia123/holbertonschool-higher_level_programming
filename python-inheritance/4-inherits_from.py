ddcc3d1ee4ad5f3077e7065e026313ea2bf05be0
Jessia123 <121071278+Jessia123@users.noreply.github.com>
2023-06-17 08:00:00 +0000

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,3 +14,4 @@ def inherits_from(obj, a_class):
     if type(obj) == a_class:
         return False
     return issubclass(type(obj), a_class)
+
--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-This module contains the method: inherits_from.
+This module contains the class: BaseGeometry.
 """
 
 
-def inherits_from(obj, a_class):
+class BaseGeometry:
     """
-    This method checks if an object, 'obj', is an instance
-    of a class that inherited (directly or indirectly) from
-    the specified class, 'a_class'. Returns boolean True or
-    False.
+    This class is currently empty.
     """
-    if type(obj) == a_class:
-        return False
-    return issubclass(type(obj), a_class)
+    pass
